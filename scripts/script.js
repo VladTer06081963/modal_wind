1ffa42eca5ea89584562585e5d047f2430b3403a
VladTer06081963 <patrol200611@ukr.net>
2023-11-08 18:57:08 +0200

    modal window

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+const modalController = ({ modal, btnOpen, btnClose }) => {
+  const buttonElems = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelector(modal);
+  modalElem.style.cssText = `
+display: flex;
+visibility: hidden;
+opacity: 0;
+transition: opacity 1300ms ease-in-out;
+`;
+
+  const closeModal = (event) => {
+    const target = event.target;
+
+    if (target === modalElem ||
+         target.closest(btnClose) ||
+         event.code === 'Escape'
+         ) {
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = "hidden";
+      }, 1300);
+      window.removeEventListener('keydown',  closeModal)
+    }
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = "visible";
+    modalElem.style.opacity = 1;
+    window.addEventListener('keydown',  closeModal)
+  };
+  // openModal();
+  buttonElems.forEach((btn) => {
+    btn.addEventListener("click", openModal);
+  });
+
+  // buttonElem.addEventListener("click", openModal);
+  modalElem.addEventListener("click", closeModal);
+};
+modalController({
+  modal: ".modal1",
+  btnOpen: ".section__button1",
+  btnClose: ".modal__close",
+});
+
+modalController({
+  modal: ".modal2",
+  btnOpen: ".section__button2",
+  btnClose: ".modal__close",
+});
